16ca5a001837dc7a5c0cefd8c7dc9570fb72352d
tunganh036 <109951935+tunganh036@users.noreply.github.com>
2025-12-18 17:18:38 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,25 +9,24 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Order Entry System",
-  description: "Professional order management interface for efficient product ordering",
-  generator: "v0.app",
-  icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
-  },
+  description: "Professional order management interface for efficient product ordering"//,
+ // icons: {
+ //   icon: [
+ //     {
+ //       url: "/icon-light-32x32.png",
+ //       media: "(prefers-color-scheme: light)",
+ //     },
+ //     {
+ //       url: "/icon-dark-32x32.png",
+ //       media: "(prefers-color-scheme: dark)",
+ //     },
+ //     {
+ //       url: "/icon.svg",
+ //       type: "image/svg+xml",
+ //     },
+ //   ],
+ //   apple: "/apple-icon.png",
+ // },
 }
 
 export default function RootLayout({
